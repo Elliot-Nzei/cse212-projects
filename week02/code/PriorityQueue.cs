9c97bffdeb2c6ddeb35010448ebf2c91e2ecb922
Elliot Ekene Nzei <elliotnzei41@gmail.com>
2025-07-18 20:26:27 +0100

    W02 Activities: Implemented Stack and Queue operations with basic tests

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -1,59 +1,31 @@
-﻿public class PriorityQueue
-{
-    private List<PriorityItem> _queue = new();
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-    /// <summary>
-    /// Add a new value to the queue with an associated priority.  The
-    /// node is always added to the back of the queue regardless of 
-    /// the priority.
-    /// </summary>
-    /// <param name="value">The value</param>
-    /// <param name="priority">The priority</param>
-    public void Enqueue(string value, int priority)
+public class PriorityQueue<T>
+{
+    private List<(T item, int priority, int index)> queue = new();
+    private int insertionIndex = 0;
+
+    public void Enqueue(T item, int priority)
     {
-        var newNode = new PriorityItem(value, priority);
-        _queue.Add(newNode);
+        queue.Add((item, priority, insertionIndex));
+        insertionIndex++;
     }
 
-    public string Dequeue()
+    public T Dequeue()
     {
-        if (_queue.Count == 0) // Verify the queue is not empty
-        {
+        if (queue.Count == 0)
             throw new InvalidOperationException("The queue is empty.");
-        }
 
-        // Find the index of the item with the highest priority to remove
-        var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
-        {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
-                highPriorityIndex = index;
-        }
+        var highest = queue
+            .OrderByDescending(x => x.priority)
+            .ThenBy(x => x.index)
+            .First();
 
-        // Remove and return the item with the highest priority
-        var value = _queue[highPriorityIndex].Value;
-        return value;
+        queue.Remove(highest);
+        return highest.item;
     }
 
-    public override string ToString()
-    {
-        return $"[{string.Join(", ", _queue)}]";
-    }
-}
-
-internal class PriorityItem
-{
-    internal string Value { get; set; }
-    internal int Priority { get; set; }
-
-    internal PriorityItem(string value, int priority)
-    {
-        Value = value;
-        Priority = priority;
-    }
-
-    public override string ToString()
-    {
-        return $"{Value} (Pri:{Priority})";
-    }
+    public int Count => queue.Count;
 }
--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -1,29 +1,40 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-// TODO Problem 2 - Write and run test cases and fix the code to match requirements.
-
 [TestClass]
-public class PriorityQueueTests
+public class PriorityQueue_Tests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    public void EnqueueAndDequeue_HighestPriorityFirst()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var pq = new PriorityQueue<string>();
+        pq.Enqueue("Low", 1);
+        pq.Enqueue("High", 5);
+        pq.Enqueue("Medium", 3);
+
+        // ✅ High should come out first
+        Assert.AreEqual("High", pq.Dequeue());
+        Assert.AreEqual("Medium", pq.Dequeue());
+        Assert.AreEqual("Low", pq.Dequeue());
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    public void SamePriority_RespectsOrderAdded()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var pq = new PriorityQueue<string>();
+        pq.Enqueue("First", 2);
+        pq.Enqueue("Second", 2);
+        pq.Enqueue("Third", 2);
+
+        // ✅ Order added should be respected
+        Assert.AreEqual("First", pq.Dequeue());
+        Assert.AreEqual("Second", pq.Dequeue());
+        Assert.AreEqual("Third", pq.Dequeue());
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+    public void EmptyQueue_ThrowsException()
+    {
+        var pq = new PriorityQueue<int>();
+        Assert.ThrowsException<InvalidOperationException>(() => pq.Dequeue());
+    }
 }
